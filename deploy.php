e139c48538ed597c863a5898fb0b42ca5a1d1210
Juozas Kaziukenas <juozas@juokaz.com>
2011-10-28 12:16:16 +0100

    yay

---
+++ deploy.php
@@ -0,0 +1,61 @@
+<?php
+
+// this is not supposed to be called from anywhere else but CLI
+if (PHP_SAPI !== 'cli') {
+    exit();
+}
+
+$folder = '.' . DIRECTORY_SEPARATOR;
+$temp_file = tempnam(sys_get_temp_dir(), 'Azurep');
+$endpoint = 'http://cloud.webspecies.co.uk/apps/pbc11';
+		
+$zip = new ZipArchive();
+
+// open archive 
+if ($zip->open($temp_file, ZIPARCHIVE::CREATE) !== TRUE) {
+	die ("Could not open archive");
+}
+
+// initialize an iterator
+// pass it the directory to be processed, which is current dir
+$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($folder, FilesystemIterator::SKIP_DOTS));
+
+// iterate over the directory
+// add each file found to the archive
+foreach ($iterator as $key=>$value) {
+	// fix file name
+	$name_ = str_replace($folder, '', $key);
+
+    // check for ignored files and folders
+    foreach (array('.git') as $pattern) {
+        if (strpos($name_, $pattern) !== false) {
+            continue 2;
+        }
+    }
+    
+	$zip->addFile(realpath($key), $name_) or die ("ERROR: Could not add file: $key");
+}
+
+// close and save archive
+$zip->close();
+
+$curl_handle = curl_init();
+    
+$headers = array(
+    'Content-Type: application/zip',
+    'Api-Key: 62967ff0308e14d8bc9ac3e76c34314ae3082dd0',
+);
+
+curl_setopt($curl_handle, CURLOPT_URL, $endpoint);
+curl_setopt($curl_handle, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl_handle, CURLOPT_POST, 1); 
+curl_setopt($curl_handle, CURLOPT_POSTFIELDS, file_get_contents($temp_file));
+
+$buffer = curl_exec($curl_handle);
+curl_close($curl_handle);
+
+print $buffer . PHP_EOL;
+
+// this file is not needed anymore
+unlink($temp_file);
---
+++ img.php
@@ -0,0 +1,70 @@
+<?php
+/*
+	(C)2007 Dustin Spicuzza
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License (GPL)
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	To read the license please visit http://www.gnu.org/copyleft/gpl.html
+
+	-----------
+	
+	Sample frontend for lolcats PHP library
+	
+*/
+
+require('lolcats.inc.php');
+
+
+$lolcat = new Lolcat();
+
+$img = '';
+$r_img = get_get_var('img');
+if ($r_img != ""){
+	// try and determine if its a URL or not
+	$url = parse_url($r_img);
+	if ($url !== false && isset($url['scheme']) && $url['scheme'] == 'http')
+		$img = $r_img;
+}
+
+if ($img == ''){
+	// pick a random one from the cats directory
+	$images = glob(dirname(__FILE__) . '/cats/*.jpg');
+	
+	// seed the generator (for static links, for example)
+	$seed = get_get_var('seed');
+	if ($seed != "")
+		mt_srand(intval($seed));
+	
+	$img = $images[mt_rand(0,count($images)-1)];
+}
+
+if (!$lolcat->Create($img,get_get_var('text'),get_get_var('align'),'cats/impact.ttf',15,300))
+	if (!headers_sent())
+		return $lolcat->ShowError("Error: I couldn't create a image for you :(");
+	else
+		return;
+
+if (!$lolcat->Show())
+	if (!headers_sent())
+		return $lolcat->ShowError("Error: I couldn't show you the image :(");
+	else
+		return;
+
+
+// utility function
+function get_get_var($str){
+	if (isset($_GET[$str]))
+		return $_GET[$str];
+	else
+		return "";
+}
+
+?>
